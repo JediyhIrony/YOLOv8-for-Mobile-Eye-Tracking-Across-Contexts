0014e14c880abbf9e49e3a3291ac2fef0d1ef6bc
JediyhIrony <129906836+JediyhIrony@users.noreply.github.com>
2025-03-27 16:50:09 -0400

    Update run_PCAT_MET.py

--- Val_Paper/MET_processing/run_PCAT_MET.py
+++ Val_Paper/MET_processing/run_PCAT_MET.py
@@ -6,11 +6,9 @@ Created on Mon Sep  2 11:18:52 2024
 """
 
 import argparse
-#from pcat_v8 import pcat_pipe, proc_met_files
 from pcat_v8 import pcat_pipe, proc_met_files
 import warnings
 parser = argparse.ArgumentParser()
-# -sub SUBJECT -vid VIDEO
 parser.add_argument("-s", "--subject", dest="subject", help="Subject Number")
 parser.add_argument("-v", "--video", action='store_true', help="Generate Video?")
 
@@ -19,14 +17,15 @@ subject = args.subject
 vidgen = args.video
 
 def main():
-    #If processing on file at a time uncomment below and comment out the many at once section
+    # If processing one file at a time uncomment below and comment out the 'many at once' section
     # global subject, vidgen, picgen
     # message = ("Proceed with processing data for subject " + str(subject) + " [y/n] \n")
     # if input(message) == "y":
     #     warnings.simplefilter("ignore")
-    #
     #     pcat_pipe(subject, vidgen)
-        subjects = ["","",""] #add subject numbers we want to process here
+
+        # If processing one file at a time, comment out below
+        subjects = ["","",""] # Add subject numbers we want to process here, for many at once
         for subject in subjects:
             warnings.simplefilter("ignore")
             print("running subject " + subject)
